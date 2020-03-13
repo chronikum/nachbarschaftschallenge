41db802e61fb3973aacfc6cea184ae6d776f4960
JonathanF <fritz@nosc.io>
2020-03-13 11:08:29 +0100

    Removed HttpClient

--- src/app/service/pdf-api.service.ts
+++ src/app/service/pdf-api.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 
